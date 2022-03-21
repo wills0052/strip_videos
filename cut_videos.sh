3dbb4c033c73d16c9dcdd3a0e9be7bb2bd3e810c
William Soo <wills0052@gmail.com>
2022-03-22 10:01:24 +1100

    Fixed cut_videos to produce warning if given arguments in wrong order

--- cut_videos.sh
+++ cut_videos.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+#set +x
 list_of_times="$1"
 video="$2"
+arg1_ext="$(echo $list_of_times | awk -F'.' '{print $NF}')"
 
-if [ "$#" -ne 2 ];
+if [[ "$#" -ne 2 ]];
 then
 	echo "Usage: ./cut_videos.sh list_of_times video"
+	exit 1
+fi
+
+if !( echo "$arg1_ext" | grep -w "vtt" ) && !( echo "arg1_ext" | grep -w "txt" );
+then
+	echo "You passed $list_of_times as the first argument."
+	echo "Ensure you have passed a transcript as the first argument."
 	exit 1
 fi
 
@@ -14,8 +23,9 @@ do
 	start_time=$(echo "$p" | awk -F';' '{print $2}')
 	end_time=$(echo "$p" | awk -F';' '{print $3}')
 
+
 	echo "Processing video $quiz_number from $start_time to $end_time"
-	#echo "ffmpeg -ss $start_time -i "$video" -to $end_time -c copy quiz_"$quiz_number"_"$start_time"_"$end_time"_"$video""
+	# echo ffmpeg -y -ss "$start_time" -to "$end_time" -i "$video" -c copy quiz_"$quiz_number"_"$start_time"_"$end_time"_"$video" -nostdin
 	ffmpeg -y -ss "$start_time" -to "$end_time" -i "$video" -c copy quiz_"$quiz_number"_"$start_time"_"$end_time"_"$video" -nostdin
 done < "$list_of_times"
 
